73241e67654e8d113fbe0c92aca0f5f7e2db05d7
Eric Fisher <eric@ericfisher.dev>
2025-07-09 16:08:05 -0500

    adding and viewing tasks

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>php-example-task-list</title>
+    </head>
+
+    <body>
+        <h1>@yield('title')</h1>
+        <div>
+            @yield('content')
+        </div>
+    </body>
+</html>
